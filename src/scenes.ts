8ba7019d9ca2ee71d63ce3fc430a77d64741490a
christiandff <161719098+christiandff@users.noreply.github.com>
2026-03-11 14:57:58 +0100

    fix

--- src/scenes.ts
+++ src/scenes.ts
@@ -1,8 +1,10 @@
-export const scenes = {
+type Scene = { background: string; width: number };
+
+export const scenes: Record<number, Scene> = {
   0: { background: "/bg/MenuBackground.png", width: 1920 },
   1: { background: "/bg/Trondheim.png",      width: 3840 },
   2: { background: "/bg/Temple.png",         width: 3840 },
   3: { background: "/bg/TempleInside.png",   width: 3840 },
   4: { background: "/bg/Diamondway.png",     width: 1920 },
   5: { background: "/bg/Nirvana.png",        width: 1920 },
-} as const;
+};
